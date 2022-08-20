e13a7972f40db33a8696a838ceb3134c7e60143d
awnish318 <awnishkumar6282@gmail.com>
2022-08-20 12:58:28 +0530

    JAVA FILE COMMITED

---
+++ GUI.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.Applet.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class GUI extends Frame implements MouseListener,MouseMotionListener{
+   Label l,l1,l2;
+   GUI(){
+   	addMouseListener(this);
+   	addMouseMotionListener(this);
+   
+   	l=new Label();
+   	l.setBounds(0,10,100,150);
+   	add(l);
+   	l1=new Label();
+   	l1.setBounds(10,90,100,150);
+   	add(l1);
+   	l2=new Label();
+   	l2.setBounds(10,180,100,150);
+   	add(l2);
+   	
+        setSize(400,400);
+        setLayout(null);
+        setVisible(true);
+   }
+   public void mouseClicked(MouseEvent e){
+   	l.setText("Mouse CLicked");
+   	System.out.println(e.getX()+""+e.getY());
+   	
+   }
+   public void mouseEntered(MouseEvent e){
+   	l.setText("Mouse Entered");
+   }
+   public void mouseexited(MouseEvent e){
+   	l2.setText("Mouse exited");
+   }
+   
+   public void mousePressed(MouseEvent e){
+   	l.setText("Mouse Pressed");
+   }
+   
+   public void mouseReleased(MouseEvent e){
+   	l2.setText("Mouse Released");
+   }
+   public void mouseDragged(MouseEvent e){
+   	l1.setText("Mouse Dragged");
+   }
+   public void mouseMoved(MouseEvent e){
+   	l1.setText("Mouse Moved");
+   }
+   public static void main(String[] args){
+   	new GUI();
+   }
+}
